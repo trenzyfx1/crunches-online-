efb8cf1ba34aa548c257bef7f7f80e1d00baea52
trenzyfx1 <rolemodel070@gmail.com>
2025-01-24 11:24:02 +0000

    demo commit

---
+++ mobile.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.getElementById ("menu-icon");
+    const menu = document.getElementById("menu");
+
+
+    menuIcon.addEventListener("click", function() {
+        menu.classList.toggle("active");
+        
+    });
+});
